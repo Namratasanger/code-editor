db8ddd3c47f15503c598e96dd4e34aab0378fc2c
namratsanger <namratasanger@namratsangers-MacBook-Air.local>
2023-04-16 15:27:14 -0400

    Initialzed the esbuild

--- src/App.js
+++ src/App.js
@@ -1,4 +1,21 @@
+import * as esbuild from "esbuild-wasm";
+import { useEffect, useRef } from "react";
+
 function App() {
+  const ref = useRef();
+
+  const startService = async () => {
+    ref.current = await esbuild.startService({
+      worker: true,
+      wasmURL: "/esbuild.wasm",
+    });
+    console.log(ref.current);
+  };
+
+  useEffect(() => {
+    startService();
+  }, []);
+
   return <div></div>;
 }
 
